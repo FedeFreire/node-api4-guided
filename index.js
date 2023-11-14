cf403b066d67135295fe2661c6e5ab35687c8715
FedeFreire <bandoleir@gmail.com>
2023-11-14 12:22:28 -0500

    dotenv added

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const server = express();
 
 server.use(express.json());
 
-server.get('/hello', (req, res) => {
-  res.json('hello, there');
+server.get("/hello", (req, res) => {
+  res.json("hello, there");
 });
 
-
-
-server.use('*', (req, res) => {
-  res.json({ message: 'API is UP!' })
+server.use("*", (req, res) => {
+  res.json({ message: "API is UP!" });
 });
 
 const port = process.env.PORT || 9000;
