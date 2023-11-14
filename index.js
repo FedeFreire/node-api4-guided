209ee831adb15a907b7edf2e9047b4288f24c7b3
FedeFreire <bandoleir@gmail.com>
2023-11-14 11:53:35 -0500

    server setup done

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ server.get('/hello', (req, res) => {
   res.json('hello, there');
 });
 
+
+
 server.use('*', (req, res) => {
   res.json({ message: 'API is UP!' })
 });
 
-server.listen(9000, () => {
-  console.log("\n*** Server Running on http://localhost:9000 ***\n");
+const port = process.env.PORT || 9000;
+
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
